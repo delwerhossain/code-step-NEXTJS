7e35856a51ddd187eecc2006a92b4bb88bc1b772
delwerhossain <delwerhossain006@gmail.com>
2023-06-24 13:02:38 +0100

    add: routes two way uses btn for test

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,29 @@
 'use client'
 
+import { useRouter } from "next/navigation";
 import Event from "./Event/Event";
+import Link from "next/link";
 
 export default function Home() {
+  const route = useRouter()
+  
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl uppercase font-bold">hello world</h1>
+    <main className="grid justify-center mt-6">
+      <h1 className="text-6xl uppercase font-bold mb-8">hello world</h1>
       <Event></Event>
-     
+      <div className="border rounded  w-96 h-96 mt-10 p-4">
+        <h2 className="text-3xl underline mb-8">routes two way uses</h2>
+        <Link className="btn btn-warning " href="/about">
+          {" "}
+          Link test{" "}
+        </Link>
+
+        <button className="btn ml-4" onClick={() => route.push("/about")}>
+          {" "}
+          Button test{" "}
+        </button>
+      </div>
     </main>
   );
 }
