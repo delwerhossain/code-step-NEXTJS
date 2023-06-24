7cf1f1d142a9dd1d1d5be8fec89fba9f81278075
delwerhossain <delwerhossain006@gmail.com>
2023-06-24 10:41:40 +0100

    Update : use component for Event

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 'use client'
+
+import Event from "./Event/Event";
+
 export default function Home() {
-  const name = () => {
-    alert('Please enter')
-  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl uppercase font-bold">hello world</h1>
-      <button className="p-5 border rounded font-semibold hover:bg-slate-600 hover:text-white" onClick={name}> click me</button>
+      <Event></Event>
+     
     </main>
   );
 }
