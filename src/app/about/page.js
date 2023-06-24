513e15bafe446ded38b8d1df15e7e86a3e38713c
delwerhossain <delwerhossain006@gmail.com>
2023-06-24 15:30:36 +0100

    add: nested route add practice

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
     <div>
       <h1 className="text-6xl">this is About page</h1>
+      <Link className=" btn mt-6" href={"/about/aboutschool"}>
+        about school
+      </Link>
     </div>
   );
 };
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,11 +1,21 @@
-import React from 'react';
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const page = () => {
-    return (
-      <div>
-        <h1 className='text-6xl'>this is Login page</h1>
-      </div>
-    );
+  const route = useRouter();
+
+  const navigate = (path) => {
+    route.push(`/login/${path}`);
+  };
+  return (
+    <div>
+      <h1 className="text-6xl">this is Login page</h1>
+      <button className="btn mt-6" onClick={()=>navigate("loginteacher")}>
+        login teacher
+      </button>
+    </div>
+  );
 };
 
 export default page;
