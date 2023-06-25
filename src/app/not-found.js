66091167d8d67f169e4ba31d3e89e998bc9ff3c8
Delwer Hossain <delwerhossain006@gmail.com>
2023-06-26 05:37:55 +0600

    add: 404 page add

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1 className='text-6xl text-white bg-red-500'>error 404</h1>
+        </div>
+    ); 
+};
+
+export default Error;
