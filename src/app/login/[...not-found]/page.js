99e18a4f5480ac2671c98167e5e946acb3ece06d
Delwer Hossain <delwerhossain006@gmail.com>
2023-06-26 05:50:42 +0600

    add: route wise 404 page

---
+++ src/app/login/[...not-found]/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import useSWR from "swr";
+
+const Error = () => {
+  return (
+    <div>
+      <h1 className="text-6xl text-white bg-red-500">error 404  ----- login</h1>
+    </div>
+  );
+};  
+ 
+export default Error;
