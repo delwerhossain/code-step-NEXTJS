e1afa36ac242b882ef4711ee7acf70b22bc4b570
Delwer Hossain <delwerhossain006@gmail.com>
2023-06-26 07:19:35 +0600

    add: server side render fetch data practice

---
+++ src/app/serverfetch/page.js
@@ -0,0 +1,20 @@
+const productList = async() => {
+    const res = await fetch("https://dummyjson.com/products");
+    const result = await res.json()
+    return result.products
+    
+}
+
+const page = async () => {
+    let products = await productList()
+    console.log(products);
+    return (
+      <div>
+        {products.map((product) => (
+          <h1 className=" ml-10" key={product.id}>products name : {product.title}</h1>
+        ))}
+      </div>
+    );
+};
+
+export default page;
